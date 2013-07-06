ace5e75da91f123b98fb3a52456bdeca944d174c
Rob Wu <gwnRob@gmail.com>
2013-07-07 00:29:43 +0200

    Update jsbeautifier dependencies

--- lib/beautify/jsbeautifier/get-jsb.sh
+++ lib/beautify/jsbeautifier/get-jsb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-wget https://raw.github.com/einars/js-beautify/master/beautify-css.js
-wget https://raw.github.com/einars/js-beautify/master/beautify-html.js
-wget https://raw.github.com/einars/js-beautify/master/beautify.js
+wget https://raw.github.com/einars/js-beautify/master/js/lib/beautify-css.js
+wget https://raw.github.com/einars/js-beautify/master/js/lib/beautify-html.js
+wget https://raw.github.com/einars/js-beautify/master/js/lib/beautify.js
 
 [ ! -d unpackers ] && mkdir unpackers
 cd unpackers
-wget https://raw.github.com/einars/js-beautify/master/unpackers/javascriptobfuscator_unpacker.js
-wget https://raw.github.com/einars/js-beautify/master/unpackers/myobfuscate_unpacker.js
-wget https://raw.github.com/einars/js-beautify/master/unpackers/p_a_c_k_e_r_unpacker.js
-wget https://raw.github.com/einars/js-beautify/master/unpackers/urlencode_unpacker.js
+wget https://raw.github.com/einars/js-beautify/master/js/lib/unpackers/javascriptobfuscator_unpacker.js
+wget https://raw.github.com/einars/js-beautify/master/js/lib/unpackers/myobfuscate_unpacker.js
+wget https://raw.github.com/einars/js-beautify/master/js/lib/unpackers/p_a_c_k_e_r_unpacker.js
+wget https://raw.github.com/einars/js-beautify/master/js/lib/unpackers/urlencode_unpacker.js

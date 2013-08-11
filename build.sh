03d5e7bb75a0bb8a4fb2115635d355147f3d6154
Rob Wu <gwnRob@gmail.com>
2013-08-11 15:07:04 +0200

    Use rsync instead of typing out all dependencies

--- build.sh
+++ build.sh
@@ -1,40 +1,15 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}" )"
-emkdir() {
-    [ ! -d "$1" ] && mkdir -p "$1"
-}
-ecp() {
-    cp -ravf $1 dist/$2
-}
 
-emkdir dist
-emkdir dist/icons
-emkdir dist/lib
-
-ecp manifest.json
-
-ecp incognito-events.js
-ecp chrome-platform-info.js
-ecp cws_pattern.js
-ecp background.js
-ecp bg-contextmenu.js
-
-ecp popup.html
-ecp popup.js
-
-ecp options.html
-ecp options.js
-
-ecp crxviewer.html
-ecp crxviewer.css
-ecp crxviewer.js
-
-ecp icons/\*.png icons/
-
-ecp lib/beautify/ lib/
-ecp lib/crx-to-zip.js lib/
-ecp lib/google-code-prettify/ lib/
-ecp lib/zip.js/ lib/
+rsync -rav \
+    manifest.json \
+    *.js \
+    *.html \
+    *.css \
+    lib \
+    icons \
+    --exclude=*.svg \
+    dist/
 
 cd dist
-7z u ../crxviewer.zip *
+7z u ../crxviewer.zip * -tzip

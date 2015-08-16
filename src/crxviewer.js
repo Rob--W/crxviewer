Rob--W/crxviewer

commit 677fe1db6be9f07e42ff812ab6031da02cc6e565

Save settings in chrome.storage.sync
Fixes #17

src/crxviewer.js
@@ -412,25 +412,38 @@ var checkAndApplyFilter = (function() {
     }
     (function() {
         // Bind to checkbox filter
+//#if CHROME || OPERA
+        var storageArea = chrome.storage.sync || chrome.storage.local;
+//#endif
 
         var FILTER_STORAGE_PREFIX = 'filter-';
         var fileList = document.getElementById('file-list');
         var checkboxes = document.querySelectorAll('input[data-filter-type]');
-        for (var i = 0; i < checkboxes.length; ++i) {
-            var checkbox = checkboxes[i];
+        [].forEach.call(checkboxes, function(checkbox) {
             var storageKey = FILTER_STORAGE_PREFIX + checkbox.dataset.filterType;
             checkbox.checked = localStorage.getItem(storageKey) !== '0';
-            checkbox.onchange = onCheckboxChange;
-            if (!checkbox.checked) {
-                fileList.classList.add('gfilter-' + checkbox.dataset.filterType);
+            checkbox.onchange = function() {
+//#if CHROME || OPERA
+                var items = {};
+                items[storageKey] = checkbox.checked;
+                storageArea.set(items);
+//#else
+                localStorage.setItem(storageKey, this.checked ? '1' : '0');
+//#endif
+            };
+//#if CHROME || OPERA
+                storageArea.get(storageKey, function(items) {
+                    checkbox.checked = items[storageKey] !== false;
+                    updateFileListView();
+                });
+//#else
+                localStorage.setItem(storageKey, this.checked ? '1' : '0');
+                updateFileListView();
+//#endif
+            function updateFileListView() {
+                fileList.classList.toggle('gfilter-' + checkbox.dataset.filterType, !checkbox.checked);
             }
-        }
-        function onCheckboxChange() {
-            // jshint validthis:true
-            var storageKey = FILTER_STORAGE_PREFIX + this.dataset.filterType;
-            localStorage.setItem(storageKey, this.checked ? '1' : '0');
-            fileList.classList.toggle('gfilter-' + this.dataset.filterType, !this.checked);
-        }
+        });
     })();
     // Bind event
     var fileFilterElem = document.getElementById('file-filter');

src/background.js
@@ -1,7 +1,7 @@
 /**
  * (c) 2013 Rob Wu <rob@robwu.nl>
  */
-/* globals chrome, cws_match_pattern, ows_match_pattern, cws_pattern, ows_pattern, URL, document, alert */
+/* globals chrome, cws_match_pattern, ows_match_pattern, cws_pattern, ows_pattern, URL, document, alert, localStorage */
 
 'use strict';
 
@@ -28,6 +28,24 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.tabs.query({url: ows_match_pattern}, queryCallback);
     function queryCallback(tabs) {
         tabs.forEach(showPageActionIfNeeded);
+    }
+    // Migrate from old localStorage settings to chrome.storage
+    var items = {};
+    Object.keys(localStorage).forEach(function(key) {
+        if (key.lastIndexOf('filter-', 0) !== 0) {
+            return;
+        }
+        var value = localStorage.getItem(key);
+        localStorage.removeItem(key);
+        if (value === '1') {
+            items[key] = true;
+        } else if (value === '0') {
+            items[key] = false;
+        }
+    });
+    if (Object.keys(items).length) {
+        var storageArea = chrome.storage.sync || chrome.storage.local;
+        storageArea.set(items);
     }
 });
 

src/bg-contextmenu.js
@@ -16,7 +16,8 @@
     chrome.runtime.onInstalled.addListener(checkContextMenuPref);
     chrome.runtime.onStartup.addListener(checkContextMenuPref);
     function checkContextMenuPref() {
-        chrome.storage.local.get({showContextMenu:true}, function(items) {
+        var storageArea = chrome.storage.sync || chrome.storage.local;
+        storageArea.get({showContextMenu:true}, function(items) {
             if (items.showContextMenu) show();
         });
     }

src/options.js
@@ -37,9 +37,10 @@ if (chrome.declarativeWebRequest) {
 }
 //#endif
 
+var storageArea = chrome.storage.sync || chrome.storage.local;
 document.getElementById('contextmenu').onchange = function() {
-    chrome.storage.local.set({showContextMenu: this.checked});
+    storageArea.set({showContextMenu: this.checked});
 };
-chrome.storage.local.get({showContextMenu:true}, function(items) {
+storageArea.get({showContextMenu:true}, function(items) {
     document.getElementById('contextmenu').checked = items.showContextMenu;
 });

commit c71bf2227886e14f88f5d8181a6074951d937c88

Fix regression: checkboxes non-functional
This bug was introduced in 677fe1db6be9f07e42ff812ab6031da02cc6e565

src/crxviewer.js
@@ -428,8 +428,9 @@ var checkAndApplyFilter = (function() {
                 items[storageKey] = checkbox.checked;
                 storageArea.set(items);
 //#else
-                localStorage.setItem(storageKey, this.checked ? '1' : '0');
+                localStorage.setItem(storageKey, checkbox.checked ? '1' : '0');
 //#endif
+                updateFileListView();
             };
 //#if CHROME || OPERA
                 storageArea.get(storageKey, function(items) {
@@ -437,7 +438,7 @@ var checkAndApplyFilter = (function() {
                     updateFileListView();
                 });
 //#else
-                localStorage.setItem(storageKey, this.checked ? '1' : '0');
+                localStorage.setItem(storageKey, checkbox.checked ? '1' : '0');
                 updateFileListView();
 //#endif
             function updateFileListView() {
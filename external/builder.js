16c694caed8e16b3c257712bccee1a329a7b6b5d
Rob Wu <rob@robwu.nl>
2016-08-31 02:56:01 -0700

    Upgrade builder.js to latest version
    1:1 copy from https://github.com/mozilla/pdf.js/blob/a4c81c203b6133ea4c73ce82a68b86f8b79b3ee8/external/builder/builder.js

--- external/builder.js
+++ external/builder.js
@@ -3,22 +3,37 @@
 
 'use strict';
 
-//require('../shelljs/make');
 var fs = require('fs'),
     path = require('path'),
     vm = require('vm');
 
 /**
- * A simple preprocessor that is based on the firefox preprocessor
- * see (https://developer.mozilla.org/en/Build/Text_Preprocessor).  The main
- * difference is that this supports a subset of the commands and it supports
- * preproccesor commands in html style comments.
- * Currently Supported commands:
+ * A simple preprocessor that is based on the Firefox preprocessor
+ * (https://dxr.mozilla.org/mozilla-central/source/build/docs/preprocessor.rst).
+ * The main difference is that this supports a subset of the commands and it
+ * supports preprocessor commands in HTML-style comments.
+ *
+ * Currently supported commands:
  * - if
+ * - elif
  * - else
  * - endif
  * - include
  * - expand
+ * - error
+ *
+ * Every #if must be closed with an #endif. Nested conditions are supported.
+ *
+ * Within an #if or #else block, one level of comment tokens is stripped. This
+ * allows us to write code that can run even without preprocessing. For example:
+ *
+ * //#if SOME_RARE_CONDITION
+ * // // Decrement by one
+ * // --i;
+ * //#else
+ * // // Increment by one.
+ * ++i;
+ * //#endif
  */
 function preprocess(inFilename, outFilename, defines) {
   // TODO make this really read line by line.
@@ -32,14 +47,39 @@ function preprocess(inFilename, outFilename, defines) {
     }
     return null;
   }
-  var writeLine = typeof outFilename === 'function' ? outFilename :
-                                                      function(line) {
-    out += line + '\n';
-  };
+  var writeLine = (typeof outFilename === 'function' ? outFilename :
+    function(line) {
+      out += line + '\n';
+    });
+  function evaluateCondition(code) {
+    if (!code || !code.trim()) {
+      throw new Error('No JavaScript expression given at ' + loc());
+    }
+    try {
+      return vm.runInNewContext(code, defines, {displayErrors: false});
+    } catch (e) {
+      throw new Error('Could not evaluate "' + code + '" at ' + loc() + '\n' +
+                      e.name + ': ' + e.message);
+    }
+  }
   function include(file) {
     var realPath = fs.realpathSync(inFilename);
     var dir = path.dirname(realPath);
-    preprocess(path.join(dir, file), writeLine, defines);
+    try {
+      var fullpath;
+      if (file.indexOf('$ROOT/') === 0) {
+        fullpath = path.join(__dirname, '../..',
+          file.substring('$ROOT/'.length));
+      } else {
+        fullpath = path.join(dir, file);
+      }
+      preprocess(fullpath, writeLine, defines);
+    } catch (e) {
+      if (e.code === 'ENOENT') {
+        throw new Error('Failed to include "' + file + '" at ' + loc());
+      }
+      throw e; // Some other error
+    }
   }
   function expand(line) {
     line = line.replace(/__[\w]+__/g, function(variable) {
@@ -52,66 +92,131 @@ function preprocess(inFilename, outFilename, defines) {
     writeLine(line);
   }
 
-  var s, state = 0, stack = [];
+  // not inside if or else (process lines)
+  var STATE_NONE = 0;
+  // inside if, condition false (ignore until #else or #endif)
+  var STATE_IF_FALSE = 1;
+  // inside else, #if was false, so #else is true (process lines until #endif)
+  var STATE_ELSE_TRUE = 2;
+  // inside if, condition true (process lines until #else or #endif)
+  var STATE_IF_TRUE = 3;
+  // inside else or elif, #if/#elif was true, so following #else or #elif is
+  // false (ignore lines until #endif)
+  var STATE_ELSE_FALSE = 4;
+
+  var line;
+  var state = STATE_NONE;
+  var stack = [];
   var control =
-    /^(?:\/\/|<!--)\s*#(if|else|endif|expand|include)(?:\s+(.*?)(?:-->)?$)?/;
+  /* jshint -W101 */
+    /^(?:\/\/|<!--)\s*#(if|elif|else|endif|expand|include|error)\b(?:\s+(.*?)(?:-->)?$)?/;
+  /* jshint +W101 */
   var lineNumber = 0;
-  while ((s = readLine()) !== null) {
+  var loc = function() {
+    return fs.realpathSync(inFilename) + ':' + lineNumber;
+  };
+  while ((line = readLine()) !== null) {
     ++lineNumber;
-    var m = control.exec(s);
+    var m = control.exec(line);
     if (m) {
       switch (m[1]) {
         case 'if':
           stack.push(state);
-          try {
-            state = vm.runInNewContext(m[2], defines) ? 3 : 1;
-          } catch (e) {
-            console.error('Could not evalute line \'' + m[2] + '\' at ' +
-                          fs.realpathSync(inFilename) + ':' + lineNumber);
-            throw e;
+          state = evaluateCondition(m[2]) ? STATE_IF_TRUE : STATE_IF_FALSE;
+          break;
+        case 'elif':
+          if (state === STATE_IF_TRUE || state === STATE_ELSE_FALSE) {
+            state = STATE_ELSE_FALSE;
+          } else if (state === STATE_IF_FALSE) {
+            state = evaluateCondition(m[2]) ? STATE_IF_TRUE : STATE_IF_FALSE;
+          } else if (state === STATE_ELSE_TRUE) {
+            throw new Error('Found #elif after #else at ' + loc());
+          } else {
+            throw new Error('Found #elif without matching #if at ' + loc());
           }
           break;
         case 'else':
-          state = state === 1 ? 3 : 2;
+          if (state === STATE_IF_TRUE || state === STATE_ELSE_FALSE) {
+            state = STATE_ELSE_FALSE;
+          } else if (state === STATE_IF_FALSE) {
+            state = STATE_ELSE_TRUE;
+          } else {
+            throw new Error('Found #else without matching #if at ' + loc());
+          }
           break;
         case 'endif':
+          if (state === STATE_NONE) {
+            throw new Error('Found #endif without #if at ' + loc());
+          }
           state = stack.pop();
           break;
         case 'expand':
-          if (state === 0 || state === 3)
+          if (state !== STATE_IF_FALSE && state !== STATE_ELSE_FALSE) {
             expand(m[2]);
+          }
           break;
         case 'include':
-          if (state === 0 || state === 3)
+          if (state !== STATE_IF_FALSE && state !== STATE_ELSE_FALSE) {
             include(m[2]);
+          }
+          break;
+        case 'error':
+          if (state !== STATE_IF_FALSE && state !== STATE_ELSE_FALSE) {
+            throw new Error('Found #error ' + m[2] + ' at ' + loc());
+          }
           break;
       }
     } else {
-      if (state === 0) {
-        writeLine(s);
-      } else if (state === 3) {
-        writeLine(s.replace(/^\/\/|^<!--|-->/g, '  '));
+      if (state === STATE_NONE) {
+        writeLine(line);
+      } else if ((state === STATE_IF_TRUE || state === STATE_ELSE_TRUE) &&
+          stack.indexOf(STATE_IF_FALSE) === -1 &&
+          stack.indexOf(STATE_ELSE_FALSE) === -1) {
+        writeLine(line.replace(/^\/\/|^<!--|-->$/g, '  '));
       }
     }
   }
-  if (state !== 0 || stack.length !== 0)
-    throw new Error('Missing endif in preprocessor.');
-  if (typeof outFilename !== 'function')
+  if (state !== STATE_NONE || stack.length !== 0) {
+    throw new Error('Missing #endif in preprocessor for ' +
+                    fs.realpathSync(inFilename));
+  }
+  if (typeof outFilename !== 'function') {
     fs.writeFileSync(outFilename, out);
+  }
 }
 exports.preprocess = preprocess;
 
+var deprecatedInMozcentral = new RegExp('(^|\\W)(' + [
+    '-moz-box-sizing',
+    '-moz-grab',
+    '-moz-grabbing'
+  ].join('|') + ')');
+
 function preprocessCSS(mode, source, destination) {
-  function hasPrefixed(line) {
+  function hasPrefixedFirefox(line) {
     return (/(^|\W)-(ms|o|webkit)-\w/.test(line));
   }
 
-  function removePrefixed(content) {
+  function hasPrefixedMozcentral(line) {
+    return (/(^|\W)-(ms|o|webkit)-\w/.test(line) ||
+            deprecatedInMozcentral.test(line));
+  }
+
+  function expandImports(content, baseUrl) {
+    return content.replace(/^\s*@import\s+url\(([^\)]+)\);\s*$/gm,
+        function(all, url) {
+      var file = path.join(path.dirname(baseUrl), url);
+      var imported = fs.readFileSync(file, 'utf8').toString();
+      return expandImports(imported, file);
+    });
+  }
+
+  function removePrefixed(content, hasPrefixedFilter) {
     var lines = content.split(/\r?\n/g);
     var i = 0;
     while (i < lines.length) {
       var line = lines[i];
-      if (!hasPrefixed(line)) {
+      if (!hasPrefixedFilter(line)) {
         i++;
         continue;
       }
@@ -151,13 +256,17 @@ function preprocessCSS(mode, source, destination) {
     return lines.join('\n');
   }
 
-  if (mode !== 'firefox') {
+  if (!mode) {
     throw new Error('Invalid CSS preprocessor mode');
   }
 
-  var content = fs.readFileSync(source, 'utf8');
-  var out = removePrefixed(content);
-  fs.writeFileSync(destination, out);
+  var content = fs.readFileSync(source, 'utf8').toString();
+  content = expandImports(content, source);
+  if (mode === 'mozcentral' || mode === 'firefox') {
+    content = removePrefixed(content, mode === 'mozcentral' ?
+                             hasPrefixedMozcentral : hasPrefixedFirefox);
+  }
+  fs.writeFileSync(destination, content);
 }
 exports.preprocessCSS = preprocessCSS;
 
@@ -186,8 +295,9 @@ function build(setup) {
     sources.forEach(function(source) {
       // ??? Warn if the source is wildcard and dest is file?
       var destWithFolder = destination;
-      if (test('-d', destination))
+      if (test('-d', destination)) {
         destWithFolder += '/' + path.basename(source);
+      }
       preprocess(source, destWithFolder, defines);
     });
   });
@@ -207,10 +317,12 @@ exports.build = build;
  */
 function merge(defaults, defines) {
   var ret = {};
-  for (var key in defaults)
+  for (var key in defaults) {
     ret[key] = defaults[key];
-  for (key in defines)
+  }
+  for (key in defines) {
     ret[key] = defines[key];
+  }
   return ret;
 }
 exports.merge = merge;

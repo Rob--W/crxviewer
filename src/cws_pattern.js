Rob--W/crxviewer

commit 7563a7542029edbaac318ad163be6eb78166ff6a

Add reviewers.addons.mozilla.org to AMO domains

src/cws_pattern.js
@@ -30,16 +30,16 @@ var ows_pattern = /^https?:\/\/addons.opera.com\/.*?extensions\/(?:details|downl
 var ows_match_pattern = '*://addons.opera.com/*extensions/details/*';
 
 // Firefox addon gallery
-var amo_pattern = /^https?:\/\/(addons\.mozilla\.org|addons(?:-dev)?\.allizom\.org)\/.*?(?:addon|review)\/([^/<>"'?#]+)/;
+var amo_pattern = /^https?:\/\/(reviewers\.)?(addons\.mozilla\.org|addons(?:-dev)?\.allizom\.org)\/.*?(?:addon|review)\/([^/<>"'?#]+)/;
 var amo_download_pattern = /^https?:\/\/(addons\.mozilla\.org|addons(?:-dev)?\.allizom\.org)\/[^?#]*\/downloads\/latest\/([^/?#]+)/;
 var amo_domain_pattern = /^https?:\/\/(addons\.mozilla\.org|addons(?:-dev)?\.allizom\.org)\//;
 var amo_match_patterns = [
     '*://addons.mozilla.org/*addon/*',
-    '*://addons.mozilla.org/*review/*',
+    '*://*.addons.mozilla.org/*review/*',
     '*://addons.allizom.org/*addon/*',
-    '*://addons.allizom.org/*review/*',
+    '*://*.addons.allizom.org/*review/*',
     '*://addons-dev.allizom.org/*addon/*',
-    '*://addons-dev.allizom.org/*review/*',
+    '*://*.addons-dev.allizom.org/*review/*',
 ];
 // Depends on: https://bugzilla.mozilla.org/show_bug.cgi?id=1620084
 var amo_xpi_cdn_pattern = /^https?:\/\/(?:addons\.cdn\.mozilla\.net|addons-dev-cdn\.allizom\.org)\/user-media\/addons\//;

commit 953cdc4d55b328edf44294f1bfa46f2103e3d072

Fix up captures in amo_pattern
`get_crx_url` expects the host name in `amo_pattern[1]` and the slug in
`amo_pattern[2]`, but 7563a7542029edbaac318ad163be6eb78166ff6a broke it.

src/cws_pattern.js
@@ -33,7 +33,7 @@ var ows_download_pattern = /^https?:\/\/addons.opera.com\/extensions\/download\/
 var ows_match_pattern = '*://addons.opera.com/*extensions/details/*';
 
 // Firefox addon gallery
-var amo_pattern = /^https?:\/\/(reviewers\.)?(addons\.mozilla\.org|addons(?:-dev)?\.allizom\.org)\/.*?(?:addon|review)\/([^/<>"'?#]+)/;
+var amo_pattern = /^https?:\/\/((?:reviewers\.)?(?:addons\.mozilla\.org|addons(?:-dev)?\.allizom\.org))\/.*?(?:addon|review)\/([^/<>"'?#]+)/;
 var amo_download_pattern = /^https?:\/\/(addons\.mozilla\.org|addons(?:-dev)?\.allizom\.org)\/[^?#]*\/downloads\/latest\/([^/?#]+)/;
 var amo_domain_pattern = /^https?:\/\/(addons\.mozilla\.org|addons(?:-dev)?\.allizom\.org)\//;
 var amo_match_patterns = [